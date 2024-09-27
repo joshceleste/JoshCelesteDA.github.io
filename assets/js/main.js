96ea7c8ac525c542530023c3039adeedda48586d
joshceleste <celeste.joshua09@gmail.com>
2024-09-27 10:42:44 +0800

    Update main.js "selector"

--- assets/js/main.js
+++ assets/js/main.js
@@ -104,7 +104,7 @@
 					overlayOpacity: 0.85,
 					popupCloserText: '',
 					popupLoaderText: '',
-					selector: '.work-item a.image',
+					selector: '.work-item a.image:not(.no-lightbox)', // Adjust the selector
 					usePopupCaption: true,
 					usePopupDefaultStyling: false,
 					usePopupEasyClose: false,
